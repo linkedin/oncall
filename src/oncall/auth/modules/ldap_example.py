0c1ae0c8417ff78843f7845b96a35e7373f0bf13
William Leese <wleese@bol.com>
2017-12-06 11:57:42 +0100

    address comments

--- src/oncall/auth/modules/ldap_example.py
+++ src/oncall/auth/modules/ldap_example.py
@@ -16,48 +16,44 @@ class Authenticator:
         if 'ldap_cert_path' in config:
             self.cert_path = config['ldap_cert_path']
             if not os.access(self.cert_path, os.R_OK):
-                logger.error("Failed to read certificate")
-                return False
+                logger.error("Failed to read ldap_cert_path certificate")
+                raise IOError
 
-        if 'ldap_bind_user' in config:
-            self.bind_user = config['ldap_bind_user']
-            self.bind_password = config['ldap_bind_password']
-            self.search_filter = config['ldap_search_filter']
+        self.bind_user = config.get('ldap_bind_user')
+        self.bind_password = config.get('ldap_bind_password')
+        self.search_filter = config.get('ldap_search_filter')
 
-        self.ldap_url = config['ldap_url']
-        self.user_suffix = config['ldap_user_suffix']
-        self.base_dn = config['ldap_base_dn']
+        self.ldap_url = config.get('ldap_url')
+        self.base_dn = config.get('ldap_base_dn')
+
+        self.user_suffix = config.get('ldap_user_suffix')
 
 
     def ldap_auth(self, username, password):
-        bind_user = username + self.user_suffix
-        bind_password = password
-
-        try:
-            ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, self.cert_path)
-            # override bind user and password if they have been specified in the config
-            bind_user = self.bind_user
-            bind_password = self.bind_password
-        except AttributeError:
-            pass
+        ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, self.cert_path)
 
         connection = ldap.initialize(self.ldap_url)
         connection.set_option(ldap.OPT_REFERRALS, 0)
 
+        if not password:
+            return False
+
+        auth_user = username + self.user_suffix
         try:
-            if password:
-                connection.simple_bind_s(bind_user, bind_password)
-                if self.bind_user:
-                    # use search filter to find DN
-                    search_filter = self.search_filter % username
-                    result = connection.search_s(self.base_dn, ldap.SCOPE_SUBTREE, search_filter, ['dn'])
-                    user_dn = result[0][0]
-                    connection.simple_bind_s(user_dn, password)
-            else:
-                return False
+            if self.bind_user:
+                # use search filter to find DN of username
+                connection.simple_bind_s(self.bind_user, self.bind_password)
+                sfilter = self.search_filter % username
+                result = connection.search_s(self.base_dn, ldap.SCOPE_SUBTREE, sfilter, ['dn'])
+                if len(result) < 1:
+                    return False
+                auth_user = result[0][0]
+
+            connection.simple_bind_s(auth_user, password)
+
         except ldap.INVALID_CREDENTIALS:
             return False
-        except ldap.SERVER_DOWN as err:
+        except (ldap.SERVER_DOWN, ldap.INVALID_DN_SYNTAX) as err:
             logger.warn("%s", err)
             return None
         return True
