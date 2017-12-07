58d7d71a2249c5ff05deb63b850660cb1d7f191b
wvl <leesew@gmail.com>
2017-12-07 03:16:39 +0100

    Add LDAP bind account with DN lookup support (#116)
    * Add LDAP bind account and DN lookup support
    Also add some logging for important errors
    
    * Change method used to check cert file readable
    
    * Re-add accidentially removed TLS
    
    * address comments

--- src/oncall/auth/modules/ldap_example.py
+++ src/oncall/auth/modules/ldap_example.py
@@ -1,33 +1,63 @@
 # Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
 # See LICENSE in the project root for license information.
 import ldap
+import os
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Authenticator:
     def __init__(self, config):
         if config.get('debug'):
             self.authenticate = self.debug_auth
             return
-        self.ldap_url = config['ldap_url']
-        self.cert_path = config['ldap_cert_path']
-        self.user_suffix = config['ldap_user_suffix']
         self.authenticate = self.ldap_auth
+
+        if 'ldap_cert_path' in config:
+            self.cert_path = config['ldap_cert_path']
+            if not os.access(self.cert_path, os.R_OK):
+                logger.error("Failed to read ldap_cert_path certificate")
+                raise IOError
+
+        self.bind_user = config.get('ldap_bind_user')
+        self.bind_password = config.get('ldap_bind_password')
+        self.search_filter = config.get('ldap_search_filter')
+
+        self.ldap_url = config.get('ldap_url')
+        self.base_dn = config.get('ldap_base_dn')
+
+        self.user_suffix = config.get('ldap_user_suffix')
+
 
     def ldap_auth(self, username, password):
         ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, self.cert_path)
+
         connection = ldap.initialize(self.ldap_url)
         connection.set_option(ldap.OPT_REFERRALS, 0)
 
+        if not password:
+            return False
+
+        auth_user = username + self.user_suffix
         try:
-            if password:
-                connection.simple_bind_s(username + self.user_suffix, password)
-            else:
-                return False
+            if self.bind_user:
+                # use search filter to find DN of username
+                connection.simple_bind_s(self.bind_user, self.bind_password)
+                sfilter = self.search_filter % username
+                result = connection.search_s(self.base_dn, ldap.SCOPE_SUBTREE, sfilter, ['dn'])
+                if len(result) < 1:
+                    return False
+                auth_user = result[0][0]
+
+            connection.simple_bind_s(auth_user, password)
+
         except ldap.INVALID_CREDENTIALS:
             return False
-        except ldap.SERVER_DOWN:
+        except (ldap.SERVER_DOWN, ldap.INVALID_DN_SYNTAX) as err:
+            logger.warn("%s", err)
             return None
         return True
+
 
     def debug_auth(self, username, password):
         return True
