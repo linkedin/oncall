f1a3e9bd21edba605102854146ff567eda043b82
William Leese <wleese@bol.com>
2018-01-05 15:38:46 +0100

    Revert "add pyflakes for tests"
    This reverts commit 1915f8fe249f82c03f538c1270fc282d066c7a4a.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setuptools.setup(
             'requests',
             'gunicorn',
             'flake8',
-            'pyflakes',
             'Sphinx==1.5.6',
             'sphinxcontrib-httpdomain',
             'sphinx_rtd_theme',
