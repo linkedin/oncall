35b7856d4ffad735b6ddd700ba69d8cce16c35c5
William Leese <wleese@bol.com>
2018-01-08 09:49:36 +0100

    Add apt-get update to CI install.sh

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bash .ci/run_mysql_docker.sh
+sudo apt-get update
 sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
 python setup.py develop
 pip install -e .[dev]
