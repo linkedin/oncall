4924eac63738cd043d8935e1f3ecb02590995fa6
William Leese <wleese@bol.com>
2018-01-08 10:41:25 +0100

    schema update

--- db/schema.v0.sql
+++ db/schema.v0.sql
@@ -296,7 +296,7 @@ CREATE TABLE IF NOT EXISTS `contact_mode` (
 -- Initialize contact modes
 -- -----------------------------------------------------
 INSERT INTO `contact_mode` (`name`, `label`)
-VALUES ('email'), ('sms'), ('call'), ('slack');
+VALUES ('email','Email'), ('sms','SMS'), ('call','Phone Number'), ('slack','Slack ID');
 
 -- -----------------------------------------------------
 -- Table `user_contact`
