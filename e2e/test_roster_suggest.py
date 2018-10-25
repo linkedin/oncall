dbd2f21176eea05156267aa38d682c03818319b6
Daniel Wang <dewang@linkedin.com>
2018-10-25 12:24:57 -0700

    Exclude busy users from roster suggest
    Also, ignore W503/W504 in flake8 checks to allow line breaks
    before or after binary operators

--- e2e/test_roster_suggest.py
+++ e2e/test_roster_suggest.py
@@ -81,3 +81,52 @@ def test_api_v0_fill_gap(user, team, role, roster, event):
                              (team_name, roster_name, role_name, start + 2000)))
     assert re.status_code == 200
     assert re.json()['user'] == user_name
+
+@prefix('test_v0_fill_gap_skip_busy')
+def test_api_v0_fill_gap_skip_busy(user, team, role, roster, event):
+    user_name = user.create()
+    user_name_2 = user.create()
+    user_name_3 = user.create()
+    user_name_4 = user.create()
+    team_name = team.create()
+    role_name = role.create()
+    role_name_2 = role.create()
+    roster_name = roster.create(team_name)
+    start = int(time.time()) + 1000
+    user.add_to_roster(user_name, team_name, roster_name)
+    user.add_to_roster(user_name_2, team_name, roster_name)
+    user.add_to_roster(user_name_3, team_name, roster_name)
+    user.add_to_roster(user_name_4, team_name, roster_name)
+
+    # Create events: user_name will be the expected user, with events far from
+    # the suggestion time (start + 2000). user_name_4 will be a busy user, who
+    # would otherwise be chosen.
+    event.create({'start': start,
+                  'end': start + 1000,
+                  'user': user_name,
+                  'team': team_name,
+                  'role': role_name})
+    event.create({'start': start + 1000,
+                  'end': start + 2000,
+                  'user': user_name_2,
+                  'team': team_name,
+                  'role': role_name})
+    event.create({'start': start + 3000,
+                  'end': start + 4000,
+                  'user': user_name_3,
+                  'team': team_name,
+                  'role': role_name})
+    event.create({'start': start + 4000,
+                  'end': start + 5000,
+                  'user': user_name,
+                  'team': team_name,
+                  'role': role_name})
+    event.create({'start': start + 1000,
+                  'end': start + 3000,
+                  'user': user_name_4,
+                  'team': team_name,
+                  'role': role_name_2})
+    re = requests.get(api_v0('teams/%s/rosters/%s/%s/suggest?start=%s' %
+                             (team_name, roster_name, role_name, start + 2000)))
+    assert re.status_code == 200
+    assert re.json()['user'] == user_name
