da187174df42074f139862c7a05fb5dd5e4573f4
Harshit Sharma <43315578+sharma1612harshit@users.noreply.github.com>
2019-02-25 16:22:38 +0530

    MS-Teams module

--- src/oncall/messengers/teams_messenger.py
+++ src/oncall/messengers/teams_messenger.py
@@ -1,4 +1,5 @@
 import pymsteams
+import logging
 from oncall.constants import TEAMS_SUPPORT
 
 class teams_messenger(object):
@@ -11,11 +12,11 @@ class teams_messenger(object):
         heading=message.get("subject")
         final_message="User: "+message.get("user")+"\nMessage: "+message.get("body")
         webhook=self.webhook
-        myTeamsMessage = pymsteams.connectorcard(webhook)
-        myTeamsMessage.title(str(heading))
-        myTeamsMessage.text(str(final_message))
-        # send the message.
-        myTeamsMessage.send()
-
-        #if error
-        #logger.info("Error while connecting to teams_messenger")
+        try:
+            myTeamsMessage = pymsteams.connectorcard(webhook)
+            myTeamsMessage.title(str(heading))
+            myTeamsMessage.text(str(final_message))
+            # send the message.
+            myTeamsMessage.send()
+        except:
+            logging.info("Some issue occured while sending the message on teams_messenger")
