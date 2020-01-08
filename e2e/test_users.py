1454735e021b0896f47a6f55526b0291faed1598
Diego Cepeda <dcepeda@linkedin.com>
2020-01-08 15:09:03 -0800

    Change encoding & collation + test  unicode name

--- e2e/test_users.py
+++ e2e/test_users.py
@@ -25,32 +25,32 @@ def test_api_v0_users():
     assert re.status_code == 200
     response = json.loads(re.text)
     assert 'contacts' in response
-    assert response['full_name'] != 'John Doe'
+    assert response['full_name'] != 'Juan Doş'
 
     # test updating user
-    re = requests.put(api_v0('users/'+user_name), json={'full_name': 'John Doe', 'time_zone': 'PDT'})
+    re = requests.put(api_v0('users/'+user_name), json={'full_name': 'Juan Doş', 'time_zone': 'PDT'})
     assert re.status_code == 204
 
     # test updating user contacts
-    re = requests.put(api_v0('users/'+user_name), json={'full_name': 'John Doe', 'contacts': {'call': '+1 333-333-3339'}})
+    re = requests.put(api_v0('users/'+user_name), json={'full_name': 'Juan Doş', 'contacts': {'call': '+1 333-333-3339'}})
     assert re.status_code == 204
 
     # make sure update has gone through, test get
     re = requests.get(api_v0('users/'+user_name))
     assert re.status_code == 200
     response = re.json()
-    assert response['full_name'] == 'John Doe'
+    assert response['full_name'] == 'Juan Doş'
 
     user_id = response['id']
     re = requests.get(api_v0('users?id=%s' % user_id))
     assert re.status_code == 200
     response = re.json()
-    assert response[0]['full_name'] == 'John Doe'
+    assert response[0]['full_name'] == 'Juan Doş'
 
     re = requests.get(api_v0('users'), params={'name': user_name, 'fields': ['full_name', 'time_zone', 'contacts']})
     assert re.status_code == 200
     response = json.loads(re.text)
-    assert response[0]['full_name'] == 'John Doe'
+    assert response[0]['full_name'] == 'Juan Doş'
     assert response[0]['time_zone'] == 'PDT'
     assert response[0]['contacts']['call'] == '+1 333-333-3339'
 
