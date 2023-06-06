b6e8b8bdd47c59c28b35bbe96bf2206f89d1ffd5
Diego Cepeda <dcepeda@linkedin.com>
2023-06-06 11:26:40 -0600

    test

--- e2e/test_teams.py
+++ e2e/test_teams.py
@@ -79,7 +79,7 @@ def test_api_v0_get_team(team, role, roster, schedule):
     team = re.json()
     assert isinstance(team, dict)
     expected_set = {'users', 'admins', 'services', 'name', 'id', 'slack_channel', 'slack_channel_notifications', 'email',
-                    'scheduling_timezone', 'iris_plan', 'iris_enabled', 'override_phone_number'}
+                    'scheduling_timezone', 'iris_plan', 'iris_enabled', 'override_phone_number', 'api_managed_roster'}
     assert expected_set == set(team.keys())
 
 
